fa7b42f92622bd52bfc3e02757e14e2db70f0887
runforest881 <yyrajat881@gmail.com>
2022-12-01 01:55:50 +0530

    pp

--- JAVA3/Setter2/src/main/java/com/usecases/CourseById.java
+++ JAVA3/Setter2/src/main/java/com/usecases/CourseById.java
@@ -1,10 +1,16 @@
 package com.usecases;
 
+import com.Dao.CourseDao;
+import com.Dao.CourseDaoImpl;
+
 import java.util.Scanner;
 
 public class CourseById {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the course_id: ");
         int id = sc.nextInt();
+
+        CourseDao dao = new CourseDaoImpl();
     }
 }

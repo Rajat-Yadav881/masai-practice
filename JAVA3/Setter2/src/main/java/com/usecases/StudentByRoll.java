8222998c95f4c652eb5376f7ee1ec3af2a550039
runforest881 <yyrajat881@gmail.com>
2022-12-01 01:57:51 +0530

    pp

--- JAVA3/Setter2/src/main/java/com/usecases/StudentByRoll.java
+++ JAVA3/Setter2/src/main/java/com/usecases/StudentByRoll.java
@@ -1,4 +1,10 @@
 package com.usecases;
 
+import java.util.Scanner;
+
 public class StudentByRoll {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }

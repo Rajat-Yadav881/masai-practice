a7486e969967fd5916c73772c60a96aa9fc3bfba
runforest881 <yyrajat881@gmail.com>
2022-12-01 01:55:06 +0530

    pp

--- JAVA3/Setter2/src/main/java/com/usecases/CourseById.java
+++ JAVA3/Setter2/src/main/java/com/usecases/CourseById.java
@@ -1,4 +1,10 @@
 package com.usecases;
 
+import java.util.Scanner;
+
 public class CourseById {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int id = sc.nextInt();
+    }
 }

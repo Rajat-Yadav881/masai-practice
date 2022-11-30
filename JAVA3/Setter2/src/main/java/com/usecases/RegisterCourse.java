37b1f869fb2effd4df4e160293a485fa5db08383
runforest881 <yyrajat881@gmail.com>
2022-12-01 01:53:16 +0530

    pp

--- JAVA3/Setter2/src/main/java/com/usecases/RegisterCourse.java
+++ JAVA3/Setter2/src/main/java/com/usecases/RegisterCourse.java
@@ -1,9 +1,26 @@
 package com.usecases;
 
+import com.Dao.CourseDao;
+import com.Dao.CourseDaoImpl;
 import com.model.Course;
+import com.model.Student;
 
 public class RegisterCourse {
     public static void main(String[] args) {
-        Course course = 
+        Course course = new Course();
+        course.setCourseName("English");
+        course.setDuration(50);
+        course.setFee(45000);
+
+        Student student = new Student();
+        student.setName("Rajat");
+        student.setEmail("bivek@gmail.com");
+        student.setMobile("4657865453");
+        student.setCourse(course);
+        course.getStudents().add(student);
+
+        CourseDao dao = new CourseDaoImpl();
+        dao.addCourse(course);
+
     }
 }

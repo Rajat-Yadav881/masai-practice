1f1c4fa53d5262da03ef10f34a8c3907915684c3
runforest881 <yyrajat881@gmail.com>
2022-12-01 01:59:07 +0530

    pp

--- JAVA3/Setter2/src/main/java/com/usecases/StudentByRoll.java
+++ JAVA3/Setter2/src/main/java/com/usecases/StudentByRoll.java
@@ -1,10 +1,23 @@
 package com.usecases;
 
+import com.Dao.StudentDao;
+import com.Dao.StudentDaoImpl;
+import com.exception.StudentNotFoundException;
+
 import java.util.Scanner;
 
 public class StudentByRoll {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Enter the roll number: ");
+        int roll = scanner.nextInt();
+
+        StudentDao dao = new StudentDaoImpl();
+        try {
+            dao.getStudent(roll);
+        } catch (StudentNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }

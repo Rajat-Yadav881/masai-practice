da03b104573e88a71e9b2b44170fe2ca52d9e0ea
runforest881 <yyrajat881@gmail.com>
2022-12-01 02:30:52 +0530

    pp

---
+++ JAVA3/setter3/src/main/java/com/utility/EMUtility.java
@@ -0,0 +1,16 @@
+package com.utility;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EMUtility {
+    private static EntityManagerFactory emf;
+    static {
+        emf = Persistence.createEntityManagerFactory("accountUnit");
+    }
+    public static EntityManager provideEntityManager(){
+       return emf.createEntityManager();
+
+    }
+}

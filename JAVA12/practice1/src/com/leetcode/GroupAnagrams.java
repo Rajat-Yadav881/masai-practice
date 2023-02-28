8e3d1199eb21cc08283caf01d608af499ecfc688
runforest881 <yyrajat881@gmail.com>
2023-02-28 20:24:05 +0530

    ok

---
+++ JAVA12/practice1/src/com/leetcode/GroupAnagrams.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+	public static void main(String[] args) {
+		String[] arr = new String[] {"eat","tea","tan","ate","nat","bat"};
+		List<List<String>> ar = groupAnagrams(arr);
+	}
+	public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<HashMap<Character, Integer>,ArrayList<String>> bmap = new HashMap<>();
+		
+        for(String str:strs) {
+        	HashMap<Character,Integer>fmap = new HashMap<>();
+        	for(int i=0;i<str.length();i++) {
+        		char ch = str.charAt(i);
+        		fmap.put(ch, fmap.getOrDefault(ch, 0)+1);
+        	}
+        	if(bmap.containsKey(fmap)==false) {
+        		ArrayList<String>list = new ArrayList<>();
+        		list.add(str);
+        		bmap.put(fmap, list);
+        	}else {
+        		ArrayList<String> list = bmap.get(fmap);
+        		list.add(str);
+        	}
+        }
+        List<List<String>> res = new ArrayList<>();
+        for(ArrayList<String> val:bmap.values()) {
+        	res.add(val);
+        }
+        return res;
+    }
+}
